07b8b0c4ca186092c45642d948e99272646ac6c0
9510626008 <mangroliyaharshil2019@gmail.com>
2025-08-01 09:26:16 +0530

    Initial commit

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from app.translator import translate_text
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
